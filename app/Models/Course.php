803eac7c6e44759c9ca5dcd37b206ff36f85682b
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-06-18 13:27:55 +0700

    setup orm course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+     protected $guarded = [
+        'id',
+    ];
 
     public function category() {
         return $this->belongsTo(Category::class, 'category_id');
--- app/Models/CourseQuestion.php
+++ app/Models/CourseQuestion.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CourseQuestion extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+     protected $guarded = [
+        'id',
+    ];
+
+    public function course() {
+        return $this->belongsTo(Course::class, 'course_id');
+    }
+
+    public function answers() {
+        return $this->hasMany(CourseAnswer::class, 'question_id', 'id');
+    }
 }
