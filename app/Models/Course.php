226f87a80ea937f55f71baf83ae8bcdb83cc2d73
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-06-18 13:40:11 +0700

    setup orm course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -21,4 +21,8 @@ class Course extends Model
     public function questions() {
         return $this->hasMany(CourseQuestion::class, 'course_id', 'id');
     }
+
+     public function students() {
+        return $this->belongsToMany(User::class, 'course_students', 'course_id', 'user_id');
+    }
 }
